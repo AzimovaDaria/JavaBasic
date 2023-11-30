45602cd3f5217bb7b52654976d63278bf7322f5f
Azimova Daria <puppybaekhyun409@gmail.com>
2023-11-30 21:24:00 +0200

    +out

---
+++ src/main/java/ua/hillel/azimova/lessons/lesson4/Main.java
@@ -0,0 +1,184 @@
+package ua.hillel.azimova.lessons.lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        System.out.println(10 + 5);
+//        System.out.println(10 - 5);
+//        System.out.println(10 * 5);
+//        System.out.println(10 / 5);
+//
+//
+//        int a = 15;
+//        int b = 5;
+////
+//        System.out.println(a + b);
+//        System.out.println(a - b);
+//        System.out.println(a * b);
+//        System.out.println(a / b);
+
+//        System.out.println(10 / 3);
+//        System.out.println(10.0 / 3);
+//        System.out.println(10 / 3.0);
+//        System.out.println(10.0 / 3.0);
+//        System.out.println(10d / 3);
+//        System.out.println(10 / 3d);
+//        System.out.println((double)10 / 3);
+//        System.out.println(10 / (double) 3);
+//        System.out.println((double) a / b);
+//        System.out.println(a / (double) b);
+//
+//        System.out.println(10 % 3); //3*3 =9; 10 - 9 = 1;
+//        System.out.println(10 % 4); //4*2 =8; 10 - 8 = 2;
+//        System.out.println(10 % 2); //2*5 = 10; 10 - 10 = 0;
+
+//        System.out.println(10 % 1); //0
+//        System.out.println(10 % 2); //0
+//        System.out.println(10 % 3); //1
+//        System.out.println(10 % 4); //2
+//        System.out.println(10 % 5); //0
+//        System.out.println(10 % 6); //4
+//        System.out.println(10 % 7); //3
+//        System.out.println(10 % 8); //2
+//        System.out.println(10 % 9); //1
+//        System.out.println(10 % 10); //0
+
+//        System.out.println(123 % 2);
+//        System.out.println(124 % 2);
+//        System.out.println(125 % 2);
+//        System.out.println(126 % 2);
+
+//
+//        int a = 10;
+//        int b = 3;
+
+//        a = a + b;
+//
+//        System.out.println(a);
+//
+//        a += b;
+//
+//        System.out.println(a);
+//
+//        a += b + 10;
+//        System.out.println(a);
+
+
+//        a += b;
+//        a -- b;
+//        a *= b;
+//        a/= b;
+//        a %= b;
+//        System.out.println(a);
+
+
+
+//        int a = 10;
+
+//        a = a +1;
+//        a +=1;
+//
+
+
+//        a++1;
+
+        // a++1; всегда добавляет только 1. а==2 нельзя.
+
+//        a++;
+
+
+//        System.out.println(++a); //10
+
+//        System.out.println(a++); //11
+//        System.out.println(a);
+//
+
+//        int a = 5;
+//        int b = 2;
+//
+////        int result = b++ - a-- + --a + ++b;
+//
+//////        System.out.println("result = " + result); //4
+////        System.out.println("a = " + a); //3 + 5
+//////        System.out.println("b = " + b); //4 + 2
+//
+//        int result = ++a + --a - b++ + a++;
+//
+//       System.out.println("result = " + result); //14
+//        System.out.println("a = " + a); //6 + 5
+//        System.out.println("b = " + b); //3 + 2
+//
+//        int a = 700;
+//        int b = 200;
+//        int c = 3500;
+//
+//        int countMonthYears = 12 * 10;
+//        int salary1 = a * countMonthYears;
+//        int salary2 = b * countMonthYears;
+//        int salary3 = c * countMonthYears;
+//        System.out.println("salary1 = " + salary1);
+//        System.out.println("salary2 = " + salary2);
+//        System.out.println("salary3 = " + salary3);
+//
+//        double tax = 1 - 0.05;
+//        double salary1WithoutTax = salary1 * tax;
+//        double salary2WithoutTax = salary2 * tax;
+//        double salary3WithoutTax = salary3 * tax;
+//        System.out.println("salary1WithoutTax = " + salary1WithoutTax);
+//        System.out.println("salary2WithoutTax = " + salary2WithoutTax);
+//        System.out.println("salary3WithoutTax = " + salary3WithoutTax);
+//
+//        double sumSalary = salary1WithoutTax + salary2WithoutTax
+//                + salary3WithoutTax;
+//        double avgSalary = sumSalary / 3;
+//        System.out.println("sumSalary = " + sumSalary);
+//        System.out.println("avgSalary = " + avgSalary);
+//
+//        System.out.println(Math.PI);
+//        System.out.println(Math.E);
+
+//        System.out.println(Math.abs(10));
+//        System.out.println(Math.abs(10.1));
+//        System.out.println(Math.abs(-10));
+//        System.out.println(Math.abs(10.0f));
+//        System.out.println(Math.abs(10L));
+//
+//        Math.pow(2, 3) //зведення числа до степені. 2 в 3 степені.
+//        System.out.println(Math.pow(2, 3));
+//        System.out.println(Math.pow(2, 4));
+//        System.out.println(Math.pow(2.2, 4.4));
+//
+//        System.out.println(Math.sqrt(9)); //корень числа
+//        System.out.println(Math.sqrt(16));
+//        System.out.println(Math.sqrt(16.5));
+//
+//        System.out.println(Math.round(10.19)); //округлює число
+//        System.out.println(Math.round(10.49));
+//        System.out.println(Math.round(10.59));
+//
+//        System.out.println(Math.min(1, 2));
+//        System.out.println(Math.min(3, 1)); // повертає менше число з 2-х чисел
+//        System.out.println(Math.min(11, 22));
+//
+//        System.out.println(Math.max(1, 2));
+//        System.out.println(Math.max(3, 1)); // повертає більше число з 2-х чисел
+//        System.out.println(Math.max(11, 22));
+//
+//        // рандомне число
+//
+//        System.out.println(Math.random()); // 0.0 - 0.99999999999
+
+        // [15 - 35]
+
+//        System.out.println(15 + (int) (Math.random() * 21));
+
+//        int min = 15;
+//        int max = 35;
+//
+////        System.out.println((int) (Math.random() * (max - min + 1) + min));
+//               //int or Math.round
+//        System.out.println(Math.round (Math.random() * (max - min + 1) + min));
+
+
+    }
+}
