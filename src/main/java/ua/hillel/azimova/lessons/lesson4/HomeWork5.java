9226d8d8e1985e2ab3b0cdd5b3bf55acc62506a4
Azimova Daria <puppybaekhyun409@gmail.com>
2023-11-30 23:02:35 +0200

    homework5

---
+++ src/main/java/ua/hillel/azimova/lessons/lesson4/HomeWork5.java
@@ -0,0 +1,21 @@
+package ua.hillel.azimova.lessons.lesson4;
+
+public class HomeWork5 {
+    public static void main(String[] args) {
+
+        int li1 = 13;
+        int li2 = 24;
+        int li3 = 46;
+
+        int min1 = 9;
+        int min2 = 35;
+        int min3 = 12;
+
+        int amountLi = (li1 + li2 + li3) * 860;
+        System.out.println(amountLi);
+
+        double amountMin = (min1 + min2 + min3) * (860 * 1.5);
+        System.out.println(Math.round(amountMin));
+
+    }
+}
