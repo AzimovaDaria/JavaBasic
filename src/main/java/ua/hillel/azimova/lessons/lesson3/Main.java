4dd5ff63c83f8722ed1323ef94db289e9fc0c1c2
Azimova Daria <puppybaekhyun409@gmail.com>
2023-11-30 21:40:23 +0200

    homework3

--- src/main/java/ua/hillel/azimova/lessons/lesson3/Main.java
+++ src/main/java/ua/hillel/azimova/lessons/lesson3/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Hello Java");
-
-
+        System.out.println("I was able to do it");
+        System.out.println("Thanks, Sensei");
 
 
 
