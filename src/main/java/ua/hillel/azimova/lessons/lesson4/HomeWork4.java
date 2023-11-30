41deca5e076073189d902520abe02b7820861f93
Azimova Daria <puppybaekhyun409@gmail.com>
2023-11-30 22:30:54 +0200

    homework3

--- src/main/java/ua/hillel/azimova/lessons/lesson4/HomeWork4.java
+++ src/main/java/ua/hillel/azimova/lessons/lesson4/HomeWork4.java
@@ -1,4 +1,20 @@
 package ua.hillel.azimova.lessons.lesson4;
 
 public class HomeWork4 {
+    public static void main(String[] args) {
+
+
+        int a = 5;
+        int b = 6;
+        int c = 9;
+
+        int volume = a * b * c;
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+
+        int length = (a + b + c) * 4;
+        System.out.println(length);
+
+
+
+    }
 }
