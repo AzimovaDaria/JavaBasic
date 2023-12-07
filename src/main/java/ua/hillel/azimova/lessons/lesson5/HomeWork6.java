72b538ba753bdd9dabb835d53cae6e3bb7542292
Azimova Daria <puppybaekhyun409@gmail.com>
2023-12-07 18:47:19 +0200

    homework6

--- src/main/java/ua/hillel/azimova/lessons/lesson5/HomeWork6.java
+++ src/main/java/ua/hillel/azimova/lessons/lesson5/HomeWork6.java
@@ -21,14 +21,39 @@ public class HomeWork6 {
 
         if (scanner.hasNextInt()) {
             playerO1 = scanner.nextInt();
+        } else {
+            System.out.println("Wrong data! Restart program!!!");
+            System.exit(0);
+        }
+
+        if (scanner.hasNextInt()) {
             playerO2 = scanner.nextInt();
+        } else {
+            System.out.println("Wrong data! Restart program!!!");
+            System.exit(0);
+        }
+
+        if (scanner.hasNextInt()) {
             playerO3 = scanner.nextInt();
+        } else {
+            System.out.println("Wrong data! Restart program!!!");
+            System.exit(0);
+        }
+
+        if (scanner.hasNextInt()) {
             playerO4 = scanner.nextInt();
+        } else {
+            System.out.println("Wrong data! Restart program!!!");
+            System.exit(0);
+        }
+
+        if (scanner.hasNextInt()) {
             playerO5 = scanner.nextInt();
         } else {
             System.out.println("Wrong data! Restart program!!!");
             System.exit(0);
         }
+
         scanner.nextLine();
 
 
@@ -45,33 +70,55 @@ public class HomeWork6 {
 
         if (scanner.hasNextInt()) {
             playerE1 = scanner.nextInt();
-            playerE2 = scanner.nextInt();
-            playerE3 = scanner.nextInt();
-            playerE4 = scanner.nextInt();
-            playerE5 = scanner.nextInt();
-        }  else {
+        } else {
             System.out.println("Wrong data! Restart program!!!");
             System.exit(0);
         }
+
+        if (scanner.hasNextInt()) {
+            playerE2 = scanner.nextInt();
+        } else {
+            System.out.println("Wrong data! Restart program!!!");
+            System.exit(0);
+        }
+
+        if (scanner.hasNextInt()) {
+            playerE3 = scanner.nextInt();
+        } else {
+            System.out.println("Wrong data! Restart program!!!");
+            System.exit(0);
+        }
+
+        if (scanner.hasNextInt()) {
+            playerE4 = scanner.nextInt();
+        } else {
+            System.out.println("Wrong data! Restart program!!!");
+            System.exit(0);
+        }
+
+        if (scanner.hasNextInt()) {
+            playerE5 = scanner.nextInt();
+        } else {
+            System.out.println("Wrong data! Restart program!!!");
+            System.exit(0);
+        }
+
         scanner.nextLine();
 
 
-//        double t1 = ((playerO1 + playerO2 + playerO3 + playerO4 + playerO5) /= 5);
-//        double t2 = ((playerE1 + playerE2 + playerE3 + playerE4 + playerE5) /= 5);
+        double t1 = ((playerO1 + playerO2 + playerO3 + playerO4 + playerO5) / 5);
+        double t2 = ((playerE1 + playerE2 + playerE3 + playerE4 + playerE5) / 5);
 
-//        System.out.println(t1);
-//        System.out.println(t2);
-//
-//
-//        if (t1 > t2) {
-//            System.out.println("Перемогла команда " + team1 + " набрала" + t1 + " очків");
-//
-//        } else if (t1 < t2) {
-//            System.out.println("Перемогла команда " + team2 + " набрала" + t2 + " очків");
-//
-//        } else if (t1 == t2) {
-//            System.out.println("Переможця немає, обидві команди набради по " + t1 + " очків");
-//        }
+
+        if (t1 > t2) {
+            System.out.println("Перемогла команда " + team1 + " набрала " + t1 + " очків");
+
+        } else if (t1 < t2) {
+            System.out.println("Перемогла команда " + team2 + " набрала " + t2 + " очків");
+
+        } else if (t1 == t2) {
+            System.out.println("Переможця немає, обидві команди набрали по " + t1 + " очків");
+        }
 
 
     }
