4a0f7323b0ca61ee260c732341bc716844711ca3
Azimova Daria <puppybaekhyun409@gmail.com>
2023-12-15 02:10:59 +0200

    /

--- src/main/java/ua/hillel/azimova/lessons/lesson6/HomeWork7.java
+++ src/main/java/ua/hillel/azimova/lessons/lesson6/HomeWork7.java
@@ -6,25 +6,32 @@ public class HomeWork7 {
 
     public static void main(String[] args) {
 
-//        Scanner scanner = new Scanner(System.in);
-//        while (true) {
-//            System.out.println("Please enter integer from 1 and 10 to win");
-//            if (scanner.hasNextInt()) {
-//                int num1 = scanner.nextInt();
-////                if (num1 <= 0 && num1 > 10) {
-//                    System.out.println("You won by picking the whole number " + num1);
-//                    break;
-//                } else {
-//                    System.out.println("Only from 1 to 10!!!");
-//                }
-//
-////            } else {
-//                System.out.println("Wrong data! Try again");
-//            }
-//            scanner.nextLine();
-//        }
+        Scanner scanner = new Scanner(System.in);
 
+        int x = 0;
+        x = (int) (Math.random() * 11);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Please enter integer from 1 and 10 to win");
+            if (scanner.hasNextInt()) {
+                int num = scanner.nextInt();
+                if (num == x) {
+                    System.out.println("You won by picking the whole number " + num);
+                    break;
+                } else if (num > 10) {
+                    System.out.println("Only from 1 to 10!!!");
+                } else {
+                    System.out.println("Nice try, but no");
+                }
+            } else {
+                System.out.println("Wrong data! Try again");
+            }
+            scanner.nextLine();
+        }
+
+
+
+
+        }
 
 
     }
-}
