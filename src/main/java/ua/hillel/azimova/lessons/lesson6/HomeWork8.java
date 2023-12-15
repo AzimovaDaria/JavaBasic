7b3e089f8d10cd0da1a6c695cfadd6e596ab38c5
Azimova Daria <puppybaekhyun409@gmail.com>
2023-12-15 02:12:18 +0200

    /

--- src/main/java/ua/hillel/azimova/lessons/lesson6/HomeWork8.java
+++ src/main/java/ua/hillel/azimova/lessons/lesson6/HomeWork8.java
@@ -6,20 +6,31 @@ public class HomeWork8 {
 
     public static void main(String[] args) {
 
-        int counter = 0;
-        for (int i = 1;  ; i++) {
-            if (i / 10 != 4 && i % 10 != 4 && i / 10 != 9 && i % 10 != 9) {
-                counter++;
-                System.out.println(i);
-            } if (counter == 100) {
-                break;
-            }
+//        int counter = 0;
+//        int x = 0;
+//        for (int i = 1; i < 100; i++) {
+////            x = i % 10;
+////            i = i / 10;
+//            if (x = i % 10 != 4 && i = i / 10) {
+//                x = i % 10;
+//                i = i / 10;
+//                System.out.println(i);
+//            }
+////            if (i % 10 != 4 && i % 10 != 9) {
+//////            System.out.println(x);
+//////            number2 = number2 / 10;)
+////                continue;
+////            }
+//            if (counter == 100) {
+//                break;
+////            counter++;
+////            System.out.println(i);
+//            }
+////            counter++;
+//////            System.out.println(i);
+////        System.out.println("counter = " + counter);
+//
 
         }
-        System.out.println("counter = " + counter);
-
-
-
-
     }
-}
+
